54c87465866f3a0a076ac937aec75a1056537c4d
Barry Gu <jianzhong.gu@harman.com>
2014-12-20 21:57:49 +0800

    add an extended macros to support more type enum element

--- cnamedenum.c
+++ cnamedenum.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 /*====================================================================================*/
 /**
@@ -54,20 +55,121 @@
 #define CountOfEnum(name) _enum##name##Size
 
 NAMED_ENUM(WeekDays, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday);
+
+
+/*********************************************************
+ *
+ * XENUM(fruits, (apple,1),(orange,3),(banana),(pear,0))
+ * --> enum fruits {elem(apple,1),elem(orange,3),elem(banana),elem(pear,0)};
+ * --> enum fruits {elem2(apple,1),elem2(orange,3),elem1(banana),elem2(pear,0)};
+ * --> enum fruits {apple=1,orange=3,banana,pear=0};
+ * --> const int fruits_size = 4;
+ * --> int fruits_value[] = {apple, orange, banana, pear};
+ * --> const char *fruits_name = {"apple", "orange", "banana", "pear"};
+ *
+ *********************************************************/
+
+#define XENUM_ITEM1(name) name
+#define XENUM_ITEM2(name, value) name = value
+
+#define XENUM_NAME1(name) name
+#define XENUM_NAME2(name, value) name
+
+#define XENUM_STRG1(name) #name
+#define XENUM_STRG2(name, value) #name
+
+#define CONCAT(A,B) CONCAT2(A, B)
+#define CONCAT2(A,B) A ## B
+
+#define XTO_STRG(...) CONCAT(XENUM_STRG, NUM_ARGS __VA_ARGS__)__VA_ARGS__
+#define XTO_NAME(...) CONCAT(XENUM_NAME, NUM_ARGS __VA_ARGS__)__VA_ARGS__
+#define XTO_ELEM(...) CONCAT(XENUM_ITEM, NUM_ARGS __VA_ARGS__)__VA_ARGS__
+
+#define XENUM(name,...) \
+	typedef enum name { FOR_EACH(XTO_ELEM,__VA_ARGS__) }name; \
+	static const int _enum##name##Size = NUM_ARGS(__VA_ARGS__);\
+    static const int _enum##name##Value[] = { FOR_EACH(XTO_NAME,__VA_ARGS__) };\
+    static const char *_enum##name##String[] = { FOR_EACH(XTO_STRG,__VA_ARGS__) };\
+    inline const char * name##ToString(const name value) \
+    { int i; for (i=0;i<_enum##name##Size;i++) {\
+		if (_enum##name##Value[i]==(int)value) {\
+			return _enum##name##String[i]; }; };\
+      return NULL; }; \
+    inline const int name##ToValue(const char *itemName, int defaultValue)\
+	{ int i; for (i=0;i<CountOfEnum(name);i++) {\
+	  if (strcmp(itemName,_enum##name##String[i]) == 0) {\
+		  return _enum##name##Value[i]; }\
+	} return defaultValue; }
+
+#define ItemNameOfEnum(enumname, itemvalue) enumname##ToString(itemvalue)
+// in this macro, set the defaultvalue to -1
+#define ItemValueOfEnum(enumname, itemname) enumname##ToValue(itemname, -1)
+
+/**
+ * due to the helper macro FE_xx is up to 8
+ * so in this sample, the most elements are
+ * supported is 8
+ * TODO: extend FE_xx to increase elements
+ */
+XENUM(Fruits,       \
+		(Apple, 1), \
+		(Orange ,3),\
+		( Banana),  \
+		( Pear, 8 ),\
+		(Cherry),   \
+		(Lemon),    \
+		(Grape),    \
+		(Peach)     \
+	 );
+
 int main(int argc, char *argv[])
 {
     WeekDays value;
+    Fruits val;
     const char *valueName;
 
     printf("there are %d of elements in enum WeekDays\n", CountOfEnum(WeekDays));
 
     value = Tuesday;
-    valueName = WeekDaysToString(value);
+    valueName = ItemNameOfEnum(WeekDays,value);
     printf("Value %d's name is :%s:\n", value, valueName);
 
     value = Friday;
-    valueName = WeekDaysToString(value);
+    valueName = ItemNameOfEnum(WeekDays,value);
     printf("Value %d's name is :%s:\n", value, valueName);
+
+    val = Apple;
+    valueName = ItemNameOfEnum(Fruits,val);
+    printf("Value %d's name is :%s:\n", val, valueName);
+
+    val = Banana;
+    valueName = ItemNameOfEnum(Fruits,val);
+    printf("Value %d's name is :%s:\n", val, valueName);
+
+    val = Pear;
+    valueName = ItemNameOfEnum(Fruits,val);
+    printf("Value %d's name is :%s:\n", val, valueName);
+
+    valueName = "banana";
+    val = ItemValueOfEnum(Fruits,valueName);
+    if (val == -1)
+    	printf("Name :%s isn't found\n", valueName);
+    else
+    	printf("Name :%s's value is %d\n", valueName, val);
+
+    valueName = "Orange";
+    val = ItemValueOfEnum(Fruits,valueName);
+    if (val == -1)
+    	printf("Name :%s: isn't found\n", valueName);
+    else
+    	printf("Name :%s's value is %d\n", valueName, val);
+
+    valueName = "Peach";
+    val = ItemValueOfEnum(Fruits,valueName);
+    if (val == -1)
+    	printf("Name :%s isn't found\n", valueName);
+    else
+    	printf("Name :%s's value is %d\n", valueName, val);
 
     return 0;
 }
